3729396152d058114e030ef48ccbbe21cdfbd177
jackp <jackpetersenmusic@gmail.com>
2022-12-30 16:28:09 -0800

    cloud

--- models/index.js
+++ models/index.js
@@ -4,17 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 console.log(config)
 let sequelize;
 console.log(config.use_env_variable)
 if (config.use_env_variable) {
+  console.log('yo')
 
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log(config.database)
 }
 
 fs
