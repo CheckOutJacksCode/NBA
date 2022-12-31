0065d0367e3ca05b0341fb8c54d601c4c2ca1259
jackp <jackpetersenmusic@gmail.com>
2022-12-30 17:23:48 -0800

    pool

--- pgPool.js
+++ pgPool.js
@@ -1,33 +1,4 @@
-
-const { Pool } = require('pg');
-const pool = new Pool();
-
-pool.on('error', (err, client) => {
-    console.error('unexpected error on idle client', err);
-    process.exit(-1);
-});
-
-pool.on('connect', (err, client) => {
-    if (err) console.error(err);
-    console.log(client);
-    console.log('successfully connected to postgres.');
-});
-
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.POSTGRESQL_ADDON_URI, {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
-/*const Pool = require('pg').Pool
-
+const Pool = require('pg').Pool
 
 const pool = new Pool({
   user: process.env.POSTGRESQL_ADDON_USER,
@@ -39,7 +10,7 @@ const pool = new Pool({
     rejectUnauthorized: false,
   }
 })
-*/
+
 module.exports = {
     query: (text, params, callback) => {
       const start = Date.now()
