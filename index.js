d9fe7b9325d27fbc1f40d026bed183a7930d79b6
jackp <jackpetersenmusic@gmail.com>
2023-01-01 11:21:20 -0800

    trying again

--- index.js
+++ index.js
@@ -80,15 +80,16 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 
-app.use(cors({
-    origin: ["http:///localhost:3000", "http:///localhost:3001", "http://app-146e2b0b-abe6-4e5f-b8c2-89d39a573bfa.cleverapps.io"],
+app.use(cors());
+/*app.use(cors({
+    origin: ["http://localhost:3001"],
     credentials: true
-}))
+}))*/
 const helmet = require('helmet')
 app.use(
     helmet.contentSecurityPolicy({
       directives: {
-        "script-src": ["'self'", "https://d3js.org/d3.v6.min.js", "http://app-146e2b0b-abe6-4e5f-b8c2-89d39a573bfa.cleverapps.io"],
+        "script-src": ["'self'", "https://d3js.org/d3.v6.min.js"],
       },
     })
   );
