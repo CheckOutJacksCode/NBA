bf968e41ae768e12aee78220d4b1d1b13325766f
jackp <jackpetersenmusic@gmail.com>
2023-01-01 12:19:45 -0800

    pool

--- pgPool.js
+++ pgPool.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.POSTGRESQL_ADDON_HOST,
   database: process.env.POSTGRESQL_ADDON_DB,
   password: process.env.POSTGRESQL_ADDON_PASSWORD,
-  port: process.env.POSTGRESQL_ADDON_PORT,
+  port: 6622,
 })
 
 module.exports = {
