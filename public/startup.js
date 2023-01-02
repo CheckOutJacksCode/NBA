5530967ff00bcde1044c4190094f89749b24bfd5
jackp <jackpetersenmusic@gmail.com>
2023-01-01 16:53:14 -0800

    boxtradish in the cloud, passport fixed

--- public/startup.js
+++ public/startup.js
@@ -431,7 +431,7 @@ const loadUpBoxScoresTraditionalLocalFunction = async() => {
 }*/
 
 const loadUpBoxScoresTraditionalLocalFunction = async() => {
-    let season = "2015-2016";
+    let season = "2016-2017";
     /*let tablelength = await getJsonResponseStartup(`/tablelength/boxscorestraditional${season}`)
     tablelength = tablelength[0].count
     let data = await getJsonResponseStartup(`/boxScoresTraditional/read/${season}`);
