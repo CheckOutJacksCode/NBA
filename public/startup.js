d429df78b57df59c31a4aa837377d8b52a4c92f0
jackp <jackpetersenmusic@gmail.com>
2023-01-03 00:00:56 -0800

    advanced

--- public/startup.js
+++ public/startup.js
@@ -396,7 +396,7 @@ const loadUpLeagueGamesBySeason = async() => {
 }
 
 const loadUpBoxScoresLocalFunction = async() => {
-    let season = "2015-2016";
+    let season = "2016-2017";
     /*let tablelength = await getJsonResponseStartup(`/tablelength/boxscores${season}`)
     console.log(tablelength)
     tablelength = tablelength[0].count
