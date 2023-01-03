f3ecaf2d58be0f46df082a24559e72ba53b5d567
jackp <jackpetersenmusic@gmail.com>
2023-01-02 16:50:09 -0800

    2021-2022

--- public/startup.js
+++ public/startup.js
@@ -431,7 +431,7 @@ const loadUpBoxScoresTraditionalLocalFunction = async() => {
 }*/
 
 const loadUpBoxScoresTraditionalLocalFunction = async() => {
-    let season = "2020-2021";
+    let season = "2021-2022";
     /*let tablelength = await getJsonResponseStartup(`/tablelength/boxscorestraditional${season}`)
     tablelength = tablelength[0].count
     let data = await getJsonResponseStartup(`/boxScoresTraditional/read/${season}`);
