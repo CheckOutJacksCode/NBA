61a885fb11128e39c20a66740e04e6caa82d5edf
jackp <jackpetersenmusic@gmail.com>
2023-02-07 13:15:29 -0800

    pool

--- pgPool.js
+++ pgPool.js
@@ -1,13 +1,18 @@
-const Pool = require('pg').Pool
+const Pool = require('pg-pool');
+const url = require('url')
 
-const pool = new Pool({
-  user: "petejackerson",
-  host: "localhost",
-  database: "nbastatistics",
-  password: "redsox45",
-  port: 5432
-})
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
 
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1]
+};
+
+const pool = new Pool(config);
 module.exports = {
     query: (text, params, callback) => {
       const start = Date.now()
