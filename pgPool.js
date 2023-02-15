0b42272b965a80396df44bb4211c2ac386e0e778
jackp <jackpetersenmusic@gmail.com>
2023-02-15 10:51:22 -0800

    readme changed

--- pgPool.js
+++ pgPool.js
@@ -3,10 +3,10 @@ const url = require('url');
 
 const config = {
   user: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT
 };
 
 const pool = new Pool(config);
